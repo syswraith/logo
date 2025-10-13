9cd402e9a2d598f082ffb82fac0bc164019d1560
syswraith <syswraith@proton.me>
2025-10-13 22:41:34 +0530

    added Parser and removed Command
    - Parser internal method creates an AST-like object
    - Parser internal method evaluates an AST-like object
    - Parser returns evaluated tokens

---
+++ src/components/Parser.ts
@@ -0,0 +1,80 @@
+export class Parser {
+  tokens: (string | number)[];
+  nested_tokens: [string, number[]][] = [];
+
+  constructor(tokens: (string | number)[]) {
+    this.tokens = tokens;
+    this.AST_constructor();
+    this.AST_evaluator();
+    this.evaluated_tokens = this.nested_tokens;
+  }
+
+  AST_constructor(): void {
+    let temp_1: [string, number[]] | null = null;
+
+    this.tokens.forEach((token) => {
+
+      if (typeof token === "string") {
+
+        if (temp_1) this.nested_tokens.push(temp_1);
+        temp_1 = [token, []];
+
+      } else if (typeof token === "number") {
+
+        if (!temp_1) throw new Error(`Number ${token} found before any command`);
+        temp_1[1].push(token);
+
+      }
+
+    });
+
+    if (temp_1) this.nested_tokens.push(temp_1);
+  }
+
+  AST_evaluator():void {
+
+    type arg_type = "number";
+
+    interface command_type {
+      command_name: string;
+      arguments_number: number;
+      arguments_type: arg_type[];
+    }
+
+    const command_definition: command_type[] = [
+      { command_name: "FD", arguments_number: 1, arguments_type: ["number"] },
+      { command_name: "BK", arguments_number: 1, arguments_type: ["number"] },
+      { command_name: "RT", arguments_number: 1, arguments_type: ["number"] },
+      { command_name: "LT", arguments_number: 1, arguments_type: ["number"] },
+      { command_name: "CMD", arguments_number: 1, arguments_type: ["number"] },
+      { command_name: "CMD", arguments_number: 2, arguments_type: ["number", "number"] },
+      { command_name: "PU", arguments_number: 0, arguments_type: [] },
+      { command_name: "PD", arguments_number: 0, arguments_type: [] },
+      { command_name: "CS", arguments_number: 0, arguments_type: [] },
+      { command_name: "HOME", arguments_number: 0, arguments_type: [] }
+    ];
+
+    this.nested_tokens.forEach(
+      (token)=>{
+        let matched = false;
+        for (const command of command_definition) {
+
+          if (command.command_name === token[0] && command.arguments_number === token[1].length) {
+            matched = true;
+            break;
+          }
+
+        }
+
+        if (!matched) {
+          throw new Error(`Invalid number of arguments (${token[1].length}) for "${token[0]}"`);
+        }
+
+      }
+    );
+    
+
+  }
+
+
+}
