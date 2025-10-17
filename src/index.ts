972eaf0d42fb6af0623c3563599757eb04f43c49
syswraith <syswraith@proton.me>
2025-10-18 01:54:20 +0530

    import issue fixes :(

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,1 @@
-import { Tokenizer } from './components/Tokenizer.ts';
-import { Parser } from './components/Parser.ts';
-
-let example = "FD 10\nFD 10\nBK 10 PD REPEAT 5 [ FD 10 ]";
-let tokenizer = new Tokenizer(example);
-let tokens = tokenizer.tokens;
-let parser = new Parser(tokens);
+import { Parser, Tokenizer } from './components';
